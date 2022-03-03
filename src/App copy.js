6698fb8819acf31777de6c6f929ef6fa3530dfe1
indieg21 <Indiagraham21@gmail.com>
2022-03-02 22:57:59 -0500

    add auth file

---
+++ src/App copy.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+i
+import Header from './Header';
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <div>
+          
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
